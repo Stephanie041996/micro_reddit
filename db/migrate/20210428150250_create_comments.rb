025613e011656aeb98a4ee9edbfd96a24dfe628a
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-04-28 18:55:14 +0200

    Update 20210428150250_create_comments.rb

--- db/migrate/20210428150250_create_comments.rb
+++ db/migrate/20210428150250_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :post_id
+      t.integer :user_id, foreign_key: true
+      t.integer :post_id, foreign_key: true
       t.text :body
       t.timestamps
     end
