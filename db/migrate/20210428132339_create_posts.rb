5f2f66a6fe9f3a2fcd5925d555e27747395b6a9d
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-04-28 18:54:00 +0200

    Update 20210428132339_create_posts.rb

--- db/migrate/20210428132339_create_posts.rb
+++ db/migrate/20210428132339_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.integer :user_id
+      t.integer :user_id, foreign_key: true
+      t.string :title
       t.string :title
       t.text :body
 
